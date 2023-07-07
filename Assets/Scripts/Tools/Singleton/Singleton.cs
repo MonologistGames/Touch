c41d7a1dc735dc021a9caba4f23bfac20309c213
Viking-wei <74452603+Viking-wei@users.noreply.github.com>
2023-07-07 23:05:59 +0800

    重力更改

---
+++ Assets/Scripts/Tools/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+namespace Tools.Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T:Component
+    {
+        private static T _instance;
+        public static T Instance => _instance;
+
+        protected virtual void Awake()
+        {
+
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+                Destroy(this.gameObject);
+        }
+    }
+}
