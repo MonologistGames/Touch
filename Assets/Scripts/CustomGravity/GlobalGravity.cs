4ffa3d3c98ba21e2392af218c2c6c09e30c33983
hayahane <02888761169@189.cn>
2023-07-08 21:46:18 +0800

    chore:remove unnecessary comments

--- Assets/Scripts/CustomGravity/GlobalGravity.cs
+++ Assets/Scripts/CustomGravity/GlobalGravity.cs
@@ -1,45 +1,18 @@
 using System;
 using UnityEngine;
 using Tools.Singleton;
-using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
-
 namespace Touch.CustomGravity
 {
-    /// <summary>
-    /// 全局状态管理 - 重力系统
-    /// </summary>
     public class GlobalGravity : Singleton<GlobalGravity>
     {
-        #region Unity 参数
-        [Tooltip("全局重力系数")]
         public float GravityFactor = 9.8f;
-        #endregion
-
-        #region 事件
-        /// <summary>
-        /// 重力（方向）更改时
-        /// </summary>
-        /// <remarks>
-        /// 在角色控制器中进行注册
-        /// </remarks>
         public event Action<Vector3> OnGravityChanged;
-        #endregion
-
-        #region 私有字段
-        private Vector3 _gravityDir = Vector3.down; // 当前重力方向
-        #endregion
-
-        #region 属性
-        /// <summary>
-        /// 获得重力方向
-        /// </summary>
+        
+        private Vector3 _gravityDir = Vector3.down;
+        
         public Vector3 GravityDirection => _gravityDir;
-        /// <summary>
-        /// 获得重力（系数乘方向）
-        /// </summary>
         public Vector3 Gravity => GravityFactor * _gravityDir;
-        #endregion
+        
         public bool ChangeDirection(Vector3 direction)
         {
             if (_gravityDir == direction) return false;
--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Touch.CustomGravity;
-using Touch.StateManager;
 
 namespace Touch.PlayerController
 {
@@ -11,72 +8,44 @@ namespace Touch.PlayerController
     [RequireComponent(typeof(PlayerInputProcessor))]
     public class PlayerController : MonoBehaviour
     {
-        #region ö��
-        /// <summary>
-        /// 玩家当前的状态
-        /// </summary>
         public enum CharacterState
         {
-            Floating, // 悬浮
-            ChangeGravity, // 更改重力中
-            Normal // 正常
+            Floating,
+            ChangeGravity,
+            Normal
         }
 
-        public CharacterState State = CharacterState.Normal;
+     
         private Animator _animator;
+        private PlayerInputProcessor _inputProcessor;
+        private Rigidbody _rigidbody;
+        
+        public float GravityFactorFactor = 1f;
+        public CharacterState State = CharacterState.Normal;
 
-        [Header("Gravity Change")] [Tooltip("当更改状态后角色残留速度")]
-        public float VelocityRemainAfterChange = 0.5f;
-        [Tooltip("更改重力的 CD")]
-        public float ChangeGravityColdTime = 0.5f;
-        [Tooltip("最大速度")]
-        [FormerlySerializedAs("VelocityLimited")]
-        public float VelocityLimit = 6f;
+        #region Gravity Change
 
         [Header("Gravity Change")]
-        [Min(0f)]
-        [Tooltip("重力系数的系数")]
-        public float GravityFactorFactor = 1f;
+        public float VelocityRemainAfterChange = 0.5f;
+        public float ChangeGravityColdTime = 0.5f;
+        private float _currentChangeGravityColdTime;
+        public float VelocityLimit = 6f;
+        [Min(0f)] public float SlowTimeScale = 0.2f;
+        
+        private bool CanChangeGravity => _currentChangeGravityColdTime <= 0f;
 
-        public float SlowTimeScale = 0.2f;
+        #endregion
 
-        [Header("Floating")]
-        [Tooltip("悬浮时最大速度")]
-        public float FloatingVelocityLimit = 1f;
-        [Tooltip("悬浮的能量")]
+        #region Floating
+
+        [Header("Floating")] public float FloatingVelocityLimit = 1f;
         public float FloatingEnergy = 3f;
-        [Tooltip("悬浮时能量回复速率")]
         public float FloatingEnergyRecoverRate = 2f;
-
-        [Header("Color")]
-        public Color GravityChangeColor;
-        public Color ChangeReadyColor;
-        public Color FloatingColor;
-
-        [Header("Component")]
-        public PlayerInputProcessor InputProcessor;
-        #endregion
-
-        #region 字段
-        private float _currentChangeGravityColdTime; // 当前冷却时间
-        private float _currentFloatingEnergy; // 当前悬浮能力
-        private float _currentSlowTimeLapse;
-        private bool _isExhausted; // 是否精疲力竭
-
-        // 组件
-        private Rigidbody _rigidbody;
-        private Material _material;
-        private Transform _transform;
-
-        // 动画状态机
-        private static readonly int Turn = Animator.StringToHash("Turn");
-        private static readonly int Slow = Animator.StringToHash("Slow");
-        #endregion
-
-        #region 私有属性
-        /// <summary>
-        /// 当前的悬浮能量
-        /// </summary>
+        private float _currentFloatingEnergy;
+        private bool _isExhausted;
+        
+        private bool CanFloat => !_isExhausted && _currentFloatingEnergy > 0f;
+        
         private float CurrentFloatingEnergy
         {
             get => _currentFloatingEnergy;
@@ -87,52 +56,41 @@ namespace Touch.PlayerController
                 OnFloatingEnergyChanged?.Invoke(_currentFloatingEnergy);
             }
         }
-        /// <summary>
-        /// 是否能改变重力
-        /// </summary>
-        private bool CanChangeGravity => _currentChangeGravityColdTime <= 0f;
-        /// <summary>
-        /// 是否能悬浮
-        /// </summary>
-        private bool CanFloat => !_isExhausted && _currentFloatingEnergy > 0f;
+        
+        #endregion
+        
+        #region Animation Params
+
+        private static readonly int Turn = Animator.StringToHash("Turn");
+        private static readonly int Slow = Animator.StringToHash("Slow");
+
         #endregion
 
-        #region 共有属性
-        /// <summary>
-        /// 重生点
-        /// </summary>
-        public Vector3 Respawn { get; set; }
-        #endregion
-
-        #region 事件
         public event Action<float> OnFloatingEnergyChanged;
-        #endregion
-
-        #region 生命周期
-        private void Awake()
-        {
-            _rigidbody = GetComponent<Rigidbody>();
-            _transform = GetComponent<Transform>();
-            CheckpointComponent.CheckpointEvent.AddListener((position) => { Respawn = position; }); // ��Ӽ���Ļص����� - ����������
-        }
 
         private void Start()
         {
+            _rigidbody = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+            _inputProcessor = GetComponent<PlayerInputProcessor>();
+            
+            // Initialize
             _currentFloatingEnergy = FloatingEnergy;
+            // Register Events
             GlobalGravity.Instance.OnGravityChanged += ChangeGravityDirection;
-            /* 注册所需保存的状态 */
-            StateManager.StateManager.Instance.RegisterStateManager(() => Respawn, (value) => _transform.position = value); // 位置（根据重生点保存）
         }
 
         private void Update()
         {
             #region Input
 
-            if (InputProcessor.GravityDirection != Vector2.zero && CanChangeGravity)
+            if (_inputProcessor.GravityDirection != Vector2.zero && CanChangeGravity)
             {
-                if (GlobalGravity.Instance.ChangeDirection(InputProcessor.GravityDirection))
+                if (GlobalGravity.Instance.ChangeDirection(_inputProcessor.GravityDirection))
                     _currentChangeGravityColdTime = ChangeGravityColdTime;
             }
+
+            #endregion
 
             #region Update Timer
 
@@ -141,7 +99,7 @@ namespace Touch.PlayerController
                 case CharacterState.Normal:
                     if (!CanChangeGravity)
                         _currentChangeGravityColdTime -= Time.unscaledDeltaTime;
-                    if (InputProcessor.IsFloating && CanFloat)
+                    if (_inputProcessor.IsFloating && CanFloat)
                     {
                         State = CharacterState.Floating;
                         _animator.SetBool(Slow, true);
@@ -160,20 +118,19 @@ namespace Touch.PlayerController
                     if (CurrentFloatingEnergy <= 0f)
                     {
                         _isExhausted = true;
-                        InputProcessor.IsFloating = false;
+                        _inputProcessor.IsFloating = false;
                         _animator.SetBool(Slow, false);
                         State = CharacterState.Normal;
                     }
 
-                    if (!InputProcessor.IsFloating)
+                    if (!_inputProcessor.IsFloating)
                     {
                         _animator.SetBool(Slow, false);
                         State = CharacterState.Normal;
                     }
+
                     break;
             }
-
-            #endregion
 
             #endregion
         }
@@ -199,28 +156,7 @@ namespace Touch.PlayerController
 
             #endregion
         }
-        #endregion
 
-        #region Unity 事件
-        /// <summary>
-        /// 碰撞检测：当碰撞到default对象时死亡
-        /// </summary>
-        /// <remarks>
-        /// 处理逻辑：
-        /// <list type="bullet">
-        /// <item><term>碰撞到<c>default</c>标签对象</term><description>角色死亡</description></item>
-        /// </list>
-        /// </remarks>
-        /// <param name="other"></param>
-        private void OnTriggerEnter(Collider other)
-        {
-            // 死亡判定
-            if (other.CompareTag("Default"))
-                Die();
-        }
-        #endregion
-
-        #region 私有方法
         private void UpdateVelocity(float velocityLimit)
         {
             var temp = _rigidbody.velocity;
@@ -243,6 +179,7 @@ namespace Touch.PlayerController
         }
 
         #region Gravity Change Effect
+
         public void SetRotationToVelocity()
         {
             transform.up = _rigidbody.velocity.normalized;
@@ -251,10 +188,8 @@ namespace Touch.PlayerController
         public void BeginChange()
         {
             _rigidbody.velocity = _rigidbody.velocity.normalized * VelocityRemainAfterChange;
-            
             Time.timeScale = SlowTimeScale;
             Time.fixedDeltaTime *= SlowTimeScale;
-
             State = CharacterState.ChangeGravity;
         }
 
@@ -262,29 +197,9 @@ namespace Touch.PlayerController
         {
             Time.timeScale = 1f;
             Time.fixedDeltaTime = 0.02f;
-
             State = CharacterState.Normal;
         }
 
-        private IEnumerator GravityChangeEffect()
-        {
-            BeginChange();
-            yield return new WaitForSecondsRealtime(_currentSlowTimeLapse);
-            EndChange();
-        }
-        #endregion
-
-        /// <summary>
-        /// 角色死亡逻辑
-        /// </summary>
-        private void Die()
-        {
-            // 读取存档点
-            StateManager.StateManager.Instance.ReadState();
-            // 重生方向和速度一样
-            _rigidbody.velocity = GlobalGravity.Instance.GravityDirection.normalized;
-            _transform.rotation = Quaternion.Euler(GlobalGravity.Instance.GravityDirection);
-        }
         #endregion
     }
 }
